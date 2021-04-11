15530760de868e534e0f605d45cead60766d328f
hadilfriaa <hadil.friaa@ynov.com>
2021-04-11 17:38:48 +0200

    Dashboard Category

---
+++ src/components/categoryAdmin/CategoriesItemAd.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="category__card" v-if="categoryObject">
+        <router-link :to="{name:'CategoryAd', params:{id:categoryObject._id}}">
+            <h2>
+                {{categoryObject.title}}
+            </h2>
+        </router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"CategoriesItemAd",
+        created() {
+            },
+        props:{
+            categoryObject: Object,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
